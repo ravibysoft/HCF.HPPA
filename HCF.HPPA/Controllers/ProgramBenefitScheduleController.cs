a47841eb0625ca9e9654df3f871b908bdb6c2539
ravibysoft <ravibysoft@gmail.com>
2024-09-24 19:34:40 +0530

    add grapecity template

--- HCF.HPPA/Controllers/ProgramBenefitScheduleController.cs
+++ HCF.HPPA/Controllers/ProgramBenefitScheduleController.cs
@@ -1,6 +1,12 @@
 ﻿using Entities.Models;
+using GrapeCity.Documents.Word;
+using GrapeCity.Documents.Word.Layout;
 using Microsoft.AspNetCore.Mvc;
 using Service.Interface;
+using System.Data;
+using System.Globalization;
+using System.IO.Compression;
+using System.Reflection;
 
 namespace HCF.HPPA.Controllers
 {
@@ -49,6 +55,77 @@ namespace HCF.HPPA.Controllers
         {
             await _service.DeleteAsync(id);
             return NoContent();
+        }
+
+        [HttpGet]
+        [Route("genrateDoc")]
+        public async Task<ActionResult> GenrateDoc()
+        {
+            var schedulelist = await _service.GetAllAsync();
+            CreateDocx(ToDataTable<ProgramBenefitSchedule>(schedulelist.ToList()));
+            return Ok("Pdf Genrate");
+        }
+
+        /// <summary>
+        /// Convert a List{T} to a DataTable.
+        /// </summary>
+        private static DataTable ToDataTable<T>(List<T> items)
+        {
+            var tb = new DataTable(typeof(T).Name);
+
+            PropertyInfo[] props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var prop in props)
+            {
+                tb.Columns.Add(prop.Name, Nullable.GetUnderlyingType(
+            prop.PropertyType) ?? prop.PropertyType);
+            }
+
+            foreach (var item in items)
+            {
+                var values = new object[props.Length];
+                for (var i = 0; i < props.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item, null);
+                }
+
+                tb.Rows.Add(values);
+            }
+
+            return tb;
+        }
+        private static GcWordDocument CreateDocx(DataTable dataTable)
+        {
+            var doc = new GcWordDocument();
+
+            // Load the template DOCX:
+            doc.Load(Path.Combine("Resources", "WordDocs", "Program.docx"));
+
+            DataTable dtProds = dataTable;
+
+            // Add the data source to the data template data sources:
+            doc.DataTemplate.DataSources.Add("hospitalName", "Hurstville Private Hospital");
+            doc.DataTemplate.DataSources.Add("hospitalName2", "Healthe Care Surgical Pty Ltd");
+            doc.DataTemplate.DataSources.Add("section", "HCF Case Payments");
+            doc.DataTemplate.DataSources.Add("effectiveDate", "Amendment effective from 1 February 2024 to 31 January 2025");
+            doc.DataTemplate.DataSources.Add("ds", dtProds);
+
+            // The document already has all the necessary bindings,
+            // so we only need to process the data template:
+            doc.DataTemplate.Process(CultureInfo.GetCultureInfo("en-US"));
+
+            using (var layout = new GcWordLayout(doc))
+            {
+                // Define the PDF output settings
+                PdfOutputSettings pdfOutputSettings = new PdfOutputSettings();
+                pdfOutputSettings.CompressionLevel = CompressionLevel.Fastest;
+                pdfOutputSettings.ConformanceLevel = GrapeCity.Documents.Pdf.PdfAConformanceLevel.PdfA1a;
+
+                // Save the Word layout as a PDF
+                layout.SaveAsPdf("ProcurementLetter.pdf", null, pdfOutputSettings);
+            }
+            // Done:
+            return doc;
         }
     }
 }
